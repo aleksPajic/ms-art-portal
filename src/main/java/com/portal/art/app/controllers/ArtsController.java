90ee4f568ef51e8dd20850d7a63cf1b8318bfa51
Aleksandra Pajic <aleksandra.pajic.96@gmail.com>
2021-01-06 13:23:54 +0100

    feature/Art-endpoints: Art creation done

--- src/main/java/com/portal/art/app/controllers/ArtsController.java
+++ src/main/java/com/portal/art/app/controllers/ArtsController.java
@@ -1,9 +1,16 @@
 package com.portal.art.app.controllers;
 
-import com.portal.art.app.repositories.dtos.ArtDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.portal.art.app.controllers.requests.ArtRequest;
 import com.portal.art.app.services.ArtService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/art")
@@ -12,8 +19,16 @@ public class ArtsController {
     @Autowired
     ArtService artService;
 
-    @PutMapping("/create")
-    public ArtDto create(@RequestBody ArtDto artDto) {
-        return artService.createArt(artDto);
+    @RequestMapping(
+            path = "/create",
+            method = RequestMethod.POST
+    )
+    public HttpStatus create(@ModelAttribute ArtRequest artRequest) {
+        try {
+            artService.createArt(artRequest);
+        } catch (IOException e) {
+            return HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+        return HttpStatus.OK;
     }
 }
--- src/main/java/com/portal/art/app/services/ArtService.java
+++ src/main/java/com/portal/art/app/services/ArtService.java
@@ -1,9 +1,14 @@
 package com.portal.art.app.services;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.portal.art.app.controllers.requests.ArtRequest;
+import com.portal.art.app.models.ArtMapper;
 import com.portal.art.app.repositories.ArtRepository;
 import com.portal.art.app.repositories.dtos.ArtDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 public class ArtService {
@@ -11,7 +16,15 @@ public class ArtService {
     @Autowired
     ArtRepository artRepository;
 
-    public ArtDto createArt(ArtDto artDto) {
+    private ArtMapper artMapper;
+
+    public ArtService() {
+        this.artMapper = new ArtMapper();
+    }
+
+    public ArtDto createArt(ArtRequest artRequest) throws IOException {
+        ArtDto artDto = artMapper.map(artRequest);
         return artRepository.save(artDto);
     }
+
 }
