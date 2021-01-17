3ee1993da2b7254928b5e9b5f82e0ab65c317b4c
Aleksandra Pajic <aleksandra.pajic.96@gmail.com>
2021-01-17 19:50:28 +0100

    TDD-approach-pagination-exhibitions: step 1. create test

---
+++ src/test/java/com/portal/art/app/services/PaginationServiceTest.java
@@ -0,0 +1,13 @@
+package com.portal.art.app.services;
+
+import org.junit.jupiter.api.Test;
+
+
+class PaginationServiceTest {
+
+    @Test
+    public void givenPageNumberSizeAndDataList_whenDataListEmpty_thenEmptyListReturned() {
+
+    }
+
+}
