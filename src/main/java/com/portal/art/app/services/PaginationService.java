5e5b0bc9a7330c6f442974cf9f721888900fbb00
Aleksandra Pajic <aleksandra.pajic.96@gmail.com>
2021-01-17 19:51:29 +0100

    feature/TDD-approach-pagination-exhibitions: step 2. write test to fail

---
+++ src/main/java/com/portal/art/app/services/PaginationService.java
@@ -0,0 +1,12 @@
+package com.portal.art.app.services;
+
+import com.portal.art.app.models.PortalPageable;
+
+import java.util.List;
+
+public class PaginationService {
+
+    public List<PortalPageable> getPageData(List<PortalPageable> pageableList, int pageNumber, int pageSize) {
+        return null;
+    }
+}
--- src/test/java/com/portal/art/app/services/PaginationServiceTest.java
+++ src/test/java/com/portal/art/app/services/PaginationServiceTest.java
@@ -1,13 +1,29 @@
 package com.portal.art.app.services;
 
+import com.portal.art.app.models.PortalPageable;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 class PaginationServiceTest {
 
     @Test
     public void givenPageNumberSizeAndDataList_whenDataListEmpty_thenEmptyListReturned() {
+        // given
+        PaginationService paginationService = new PaginationService();
+        int pageNumber = 1;
+        int pageSize = 10;
+        List<PortalPageable> pageableList = new ArrayList<>();
 
+        // when
+        List<PortalPageable> pageData = paginationService.getPageData(pageableList, pageNumber, pageSize);
+
+        // then
+        assertThat(pageData).isEmpty();
     }
 
 }
