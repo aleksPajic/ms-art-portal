465d5a38ccb2e89114d23858ec66e3621ede5317
Aleksandra Pajic <aleksandra.pajic.96@gmail.com>
2021-01-17 20:29:23 +0100

    feature/TDD-approach-pagination-exhibitions: step 3. write code for test to pass

--- src/main/java/com/portal/art/app/services/PaginationService.java
+++ src/main/java/com/portal/art/app/services/PaginationService.java
@@ -8,6 +8,9 @@ import java.util.List;
 public class PaginationService {
 
     public List<PortalPageable> getPageData(List<PortalPageable> pageableList, int pageNumber, int pageSize) {
+        if (pageNumber < 0) {
+           throw new IllegalArgumentException();
+        }
         return Collections.emptyList();
     }
 }
