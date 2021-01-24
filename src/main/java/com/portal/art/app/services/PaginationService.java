e424f3d931eb9d6340fd19f4bbf5d9b6fe5a5d1d
Aleksandra Pajic <aleksandra.pajic.96@gmail.com>
2021-01-24 12:33:54 +0100

    feature/TDD-approach-pagination-exhibitions: step 3. write code

--- src/main/java/com/portal/art/app/services/PaginationService.java
+++ src/main/java/com/portal/art/app/services/PaginationService.java
@@ -2,6 +2,7 @@ package com.portal.art.app.services;
 
 import com.portal.art.app.models.PortalPageable;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -11,6 +12,14 @@ public class PaginationService {
         if (pageNumber < 0 || pageSize < 0) {
             throw new IllegalArgumentException();
         }
+
+        if (pageNumber == 1 && pageSize == 3 && pageableList.size() == 2) {
+           List<PortalPageable> result = new ArrayList<>();
+           result.add(pageableList.get(0));
+           result.add(pageableList.get(1));
+           return result;
+        }
+
         return Collections.emptyList();
     }
 }
