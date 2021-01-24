9e7b02c5f01587328e9a73b4f966401b5e569e5d
Aleksandra Pajic <aleksandra.pajic.96@gmail.com>
2021-01-24 16:16:00 +0100

    feature/TDD-approach-pagination-exhibitions: step 3. write code for test to pass

--- src/main/java/com/portal/art/app/services/PaginationService.java
+++ src/main/java/com/portal/art/app/services/PaginationService.java
@@ -10,6 +10,9 @@ import java.util.List;
 public class PaginationService {
 
     public List<PortalPageable> getPageData(List<PortalPageable> pageableList, int pageNumber, int pageSize) throws ArgumentOutOfRangeException {
+        if (pageableList == null) {
+            throw new IllegalArgumentException();
+        }
         if (pageNumber < 0 || pageSize < 0) {
             throw new IllegalArgumentException();
         }
