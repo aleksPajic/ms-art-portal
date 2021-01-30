5a48da91b65114f3da18497754f3f4259b28cb47
Aleksandra Pajic <aleksandra.pajic.96@gmail.com>
2021-01-30 15:26:19 +0100

    feature/TDD-approach-search-arts: step 2. write test

--- src/test/java/com/portal/art/app/services/SearchArtsServiceTest.java
+++ src/test/java/com/portal/art/app/services/SearchArtsServiceTest.java
@@ -4,7 +4,10 @@ import com.portal.art.app.models.Art;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 class SearchArtsServiceTest {
@@ -27,7 +30,18 @@ class SearchArtsServiceTest {
 
     @Test
     public void givenDataListEmptyAndSearchParameters_whenSearchArtsCalled_thenReturnEmptyArray() {
-        
+        //given
+        SearchArtsService searchArtsService = new SearchArtsService();
+        List<Art> arts = new ArrayList<>();
+        String technique = "technique";
+        String name = "name";
+        String artist = "artist";
+
+        //when
+        List<Art> result = searchArtsService.searchArts(arts, technique, name, artist);
+
+        //then
+        assertThat(result).isEmpty();
     }
 
 }
