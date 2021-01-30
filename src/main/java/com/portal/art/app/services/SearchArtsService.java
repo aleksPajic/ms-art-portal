6cf518d26484f0b151dc8eb0e45b8f04b80fe945
Aleksandra Pajic <aleksandra.pajic.96@gmail.com>
2021-01-30 15:28:55 +0100

    feature/TDD-approach-search-arts: step 3. write code for test to pass

--- src/main/java/com/portal/art/app/services/SearchArtsService.java
+++ src/main/java/com/portal/art/app/services/SearchArtsService.java
@@ -2,11 +2,15 @@ package com.portal.art.app.services;
 
 import com.portal.art.app.models.Art;
 
+import java.util.Collections;
 import java.util.List;
 
 public class SearchArtsService {
 
     public List<Art> searchArts(List<Art> arts, String technique, String name, String artist) {
+        if(arts != null && arts.isEmpty()) {
+            return Collections.emptyList();
+        }
         throw new IllegalArgumentException();
     }
 }
