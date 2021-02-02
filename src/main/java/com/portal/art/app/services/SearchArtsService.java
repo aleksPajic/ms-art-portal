7391cc4ded845f09547181552089e0b351786185
Aleksandra Pajic <aleksandra.pajic.96@gmail.com>
2021-02-02 19:28:29 +0100

    feature/TDD-approach-search-arts: step 3. write code for test to pass

--- src/main/java/com/portal/art/app/services/SearchArtsService.java
+++ src/main/java/com/portal/art/app/services/SearchArtsService.java
@@ -3,6 +3,7 @@ package com.portal.art.app.services;
 import com.portal.art.app.models.Art;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class SearchArtsService {
 
@@ -14,6 +15,17 @@ public class SearchArtsService {
         if((Objects.isNull(technique) || technique.isEmpty()) && (Objects.isNull(name) || name.isEmpty()) &&
                 (Objects.isNull(artist) || artist.isEmpty())) {
             return arts;
+        }
+
+        if (name == null && technique != null && !technique.isEmpty() && artist != null && !artist.isEmpty()) {
+            List<Art> result = arts.stream().filter((art) -> {
+                boolean artistExist = art.getArtist_username().contains(artist);
+                boolean techniqueExists = art.getTechniques()
+                        .stream()
+                        .anyMatch(x -> x.contains(technique));
+                return artistExist && techniqueExists;
+            }).collect(Collectors.toList());
+            return result;
         }
 
         if (name != null && !name.isEmpty()) {
