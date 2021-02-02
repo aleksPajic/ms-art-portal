5b13a7c4393ac1c4a7a85e12b950fb6b33d33228
Aleksandra Pajic <aleksandra.pajic.96@gmail.com>
2021-02-02 19:35:25 +0100

    feature/TDD-approach-search-arts: refactor code

--- src/main/java/com/portal/art/app/services/SearchArtsService.java
+++ src/main/java/com/portal/art/app/services/SearchArtsService.java
@@ -8,60 +8,39 @@ import java.util.stream.Collectors;
 public class SearchArtsService {
 
     public List<Art> searchArts(List<Art> arts, String technique, String name, String artist) {
-        if(arts == null) {
+        if (arts == null) {
             throw new IllegalArgumentException();
         }
 
-        if((Objects.isNull(technique) || technique.isEmpty()) && (Objects.isNull(name) || name.isEmpty()) &&
+        if ((Objects.isNull(technique) || technique.isEmpty()) && (Objects.isNull(name) || name.isEmpty()) &&
                 (Objects.isNull(artist) || artist.isEmpty())) {
             return arts;
         }
 
         if (name == null && technique != null && !technique.isEmpty() && artist != null && !artist.isEmpty()) {
-            List<Art> result = arts.stream().filter((art) -> {
+            return arts.stream().filter((art) -> {
                 boolean artistExist = art.getArtist_username().contains(artist);
                 boolean techniqueExists = art.getTechniques()
                         .stream()
                         .anyMatch(x -> x.contains(technique));
                 return artistExist && techniqueExists;
             }).collect(Collectors.toList());
-            return result;
         }
 
         if (name != null && !name.isEmpty()) {
-            List<Art> result = new ArrayList<>();
-            for (Art item : arts) {
-                if (item.getName().contains(name)) {
-                    result.add(item);
-                }
-            }
-            return result;
+            return arts.stream()
+                    .filter(art -> art.getName().contains(name))
+                    .collect(Collectors.toList());
         }
 
         if (artist != null && !artist.isEmpty()) {
-            List<Art> result = new ArrayList<>();
-            for (Art item : arts) {
-                if (item.getArtist_username().contains(artist)) {
-                    result.add(item);
-                }
-            }
-            return result;
+            return arts.stream()
+                    .filter(art -> art.getArtist_username().contains(artist))
+                    .collect(Collectors.toList());
         }
 
-        if (technique != null && !technique.isEmpty()) {
-            List<Art> result = new ArrayList<>();
-            for (Art item : arts) {
-                Optional<String> techniqueOptional = item.getTechniques()
-                        .stream()
-                        .filter(x -> x.contains(technique))
-                        .findAny();
-                if (techniqueOptional.isPresent()) {
-                    result.add(item);
-                }
-            }
-            return result;
-        }
-
-        return Collections.emptyList();
+        return arts.stream()
+                .filter(art -> art.getTechniques().stream().anyMatch(x -> x.contains(technique)))
+                .collect(Collectors.toList());
     }
 }
