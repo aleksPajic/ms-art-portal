6b3218dfee7c2e8b10ec073820868d36ba1ca50e
Aleksandra Pajic <aleksandra.pajic.96@gmail.com>
2021-02-02 20:15:03 +0100

    feature/TDD-approach-search-arts: step 2. write test

--- src/test/java/com/portal/art/app/services/SearchArtsServiceTest.java
+++ src/test/java/com/portal/art/app/services/SearchArtsServiceTest.java
@@ -149,7 +149,7 @@ class SearchArtsServiceTest {
         List<Art> arts = new ArrayList<>();
         arts.add(new Art(String.valueOf(0), "name0", "artistTest0", List.of("technique0")));
         arts.add(new Art(String.valueOf(1), "name test 1", "artistTest2", List.of("acrylics", "technique2")));
-        arts.add(new Art(String.valueOf(2), "name test 2", "artist2", List.of("technique2", "acrylics")));
+        arts.add(new Art(String.valueOf(2), "nameTest 2", "artist2", List.of("technique2", "acrylics")));
         arts.add(new Art(String.valueOf(3), "name3", "artistTest_test3", List.of("technique3")));
 
         //when
@@ -164,7 +164,25 @@ class SearchArtsServiceTest {
 
     @Test
     public void givenDataListFullAndArtistParameterValueNull_whenSearchArtsCalled_thenDataListIsFilteredByTechniqueAndName() {
+        //given
+        initSearchParameters("acrylics", "test", null);
+        List<Art> arts = new ArrayList<>();
+        arts.add(new Art(String.valueOf(0), "name0", "artistTest0", List.of("technique0")));
+        arts.add(new Art(String.valueOf(1), "name test 1", "artistTest2", List.of("acrylics", "technique2")));
+        arts.add(new Art(String.valueOf(2), "nameTest 2", "artist2", List.of("technique2", "acrylics")));
+        arts.add(new Art(String.valueOf(3), "name3", "artistTest_test3", List.of("technique3")));
 
+        //when
+        List<Art> result = searchArtsService.searchArts(arts, technique, name, artist);
+
+        //then
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result.get(0).getId()).isEqualTo("1");
+        assertThat(result.get(0).getTechniques()).contains("acrylics");
+        assertThat(result.get(0).getName()).isEqualTo("name test 1");
+        assertThat(result.get(1).getId()).isEqualTo("2");
+        assertThat(result.get(1).getTechniques()).contains("acrylics");
+        assertThat(result.get(1).getName()).isEqualTo("nameTest 2");
     }
 
     private void initSearchParameters(String technique, String name, String artist) {
