195003e7c908064fe383c6971a6d43bbc150df7a
Aleksandra Pajic <aleksandra.pajic.96@gmail.com>
2021-02-06 22:29:26 +0100

    feature/TDD-approach-ArtService: refactor tests

--- src/test/java/com/portal/art/app/services/ArtServiceTest.java
+++ src/test/java/com/portal/art/app/services/ArtServiceTest.java
@@ -2,7 +2,6 @@ package com.portal.art.app.services;
 
 import com.portal.art.app.common.exceptions.ArgumentOutOfRangeException;
 import com.portal.art.app.models.Art;
-import com.portal.art.app.models.ArtMapper;
 import com.portal.art.app.repositories.ArtRepository;
 import com.portal.art.app.repositories.dtos.ArtDto;
 import org.junit.jupiter.api.Test;
@@ -18,86 +17,43 @@ class ArtServiceTest {
     @Test
     void givenSearchParameters_whenSearchForArtsCalled_thenAllArtsFromRepositoryAreRetrieved() {
         //given
-        String tehnique = "technique";
+        String technique = "technique";
         String name = "name";
         String artist = "artist";
-        List<ArtDto> dtos = new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
-            dtos.add(new ArtDto());
-        }
-
-        List<Art> artModels = new ArrayList<>();
-        artModels.add(createArtModel("0", "name0", "artist1", List.of("technique")));
-        artModels.add(createArtModel("1", "test0", "art_test1", List.of("tec_test")));
-        artModels.add(createArtModel("2", "name2", "artist2", List.of("technique2")));
+        List<ArtDto> artDtoModels = new ArrayList<>();
+        artDtoModels.add(createArtModel("0", "name0", "artist1", List.of("technique")));
+        artDtoModels.add(createArtModel("1", "test0", "art_test1", List.of("tec_test")));
+        artDtoModels.add(createArtModel("2", "name2", "artist2", List.of("technique2")));
 
         ArtRepository artRepositoryMock = mock(ArtRepository.class);
-        when(artRepositoryMock.findAll()).thenReturn(dtos);
-        ArtMapper artMapperMock = mock(ArtMapper.class);
-        when(artMapperMock.map(dtos)).thenReturn(artModels);
-        ArtService artService = new ArtService(artRepositoryMock, artMapperMock);
+        when(artRepositoryMock.findAll()).thenReturn(artDtoModels);
+        ArtService artService = new ArtService(artRepositoryMock);
 
         //when
-        artService.searchForArts(tehnique, name, artist);
+        artService.searchForArts(technique, name, artist);
 
         //then
         verify(artRepositoryMock, times(1)).findAll();
     }
 
     @Test
-    void givenSearchParameters_whenSearchForArtsCalled_thenAllArtDtosAreMappedToArtModel() {
-        //given
-        String tehnique = "technique";
-        String name = "name";
-        String artist = "artist";
-        List<ArtDto> dtos = new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
-            dtos.add(new ArtDto());
-        }
-
-        List<Art> artModels = new ArrayList<>();
-        artModels.add(createArtModel("0", "name0", "artist1", List.of("technique")));
-        artModels.add(createArtModel("1", "test0", "art_test1", List.of("tec_test")));
-        artModels.add(createArtModel("2", "name2", "artist2", List.of("technique2")));
-
-        ArtRepository artRepositoryMock = mock(ArtRepository.class);
-        when(artRepositoryMock.findAll()).thenReturn(dtos);
-        ArtMapper artMapperMock = mock(ArtMapper.class);
-        when(artMapperMock.map(dtos)).thenReturn(artModels);
-        ArtService artService = new ArtService(artRepositoryMock, artMapperMock);
-
-        //when
-        artService.searchForArts(tehnique, name, artist);
-
-        //then
-        verify(artMapperMock, times(1)).map(eq(dtos));
-    }
-
-    @Test
     void givenSearchParameters_whenSearchForArtsCalled_thenFilteredArtModelsAreReturned() {
         //given
-        String tehnique = "technique";
+        String technique = "technique";
         String name = "name";
         String artist = "artist";
 
-        List<ArtDto> dtos = new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
-            dtos.add(new ArtDto());
-        }
-
-        List<Art> artModels = new ArrayList<>();
-        artModels.add(createArtModel("0", "name0", "artist1", List.of("technique")));
-        artModels.add(createArtModel("1", "test0", "art_test1", List.of("tec_test")));
-        artModels.add(createArtModel("2", "name2", "artist2", List.of("technique2")));
+        List<ArtDto> artDtoModels = new ArrayList<>();
+        artDtoModels.add(createArtModel("0", "name0", "artist1", List.of("technique")));
+        artDtoModels.add(createArtModel("1", "test0", "art_test1", List.of("tec_test")));
+        artDtoModels.add(createArtModel("2", "name2", "artist2", List.of("technique2")));
 
         ArtRepository artRepositoryMock = mock(ArtRepository.class);
-        when(artRepositoryMock.findAll()).thenReturn(dtos);
-        ArtMapper artMapperMock = mock(ArtMapper.class);
-        when(artMapperMock.map(dtos)).thenReturn(artModels);
-        ArtService artService = new ArtService(artRepositoryMock, artMapperMock);
+        when(artRepositoryMock.findAll()).thenReturn(artDtoModels);
+        ArtService artService = new ArtService(artRepositoryMock);
 
         //when
-        List<Art> result = artService.searchForArts(tehnique, name, artist);
+        List<Art> result = artService.searchForArts(technique, name, artist);
 
         //then
         assertThat(result.size()).isEqualTo(2);
@@ -108,36 +64,34 @@ class ArtServiceTest {
     @Test
     void givenSearchAndPageParameters_whenGetSearchArtsForPageCalled_thenFilteredArtModelsForPageAreReturned() throws ArgumentOutOfRangeException {
         //given
-        String tehnique = "technique";
+        String technique = "technique";
         String name = "name";
         String artist = "artist";
         int pageNumber = 1;
         int pageSize = 1;
 
-        List<ArtDto> dtos = new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
-            dtos.add(new ArtDto());
-        }
-
-        List<Art> artModels = new ArrayList<>();
-        artModels.add(createArtModel("0", "name0", "artist1", List.of("technique")));
-        artModels.add(createArtModel("1", "test0", "art_test1", List.of("tec_test")));
-        artModels.add(createArtModel("2", "name2", "artist2", List.of("technique2")));
+        List<ArtDto> artDtoModels = new ArrayList<>();
+        artDtoModels.add(createArtModel("0", "name0", "artist1", List.of("technique")));
+        artDtoModels.add(createArtModel("1", "test0", "art_test1", List.of("tec_test")));
+        artDtoModels.add(createArtModel("2", "name2", "artist2", List.of("technique2")));
 
         ArtRepository artRepositoryMock = mock(ArtRepository.class);
-        when(artRepositoryMock.findAll()).thenReturn(dtos);
-        ArtMapper artMapperMock = mock(ArtMapper.class);
-        when(artMapperMock.map(dtos)).thenReturn(artModels);
-        ArtService artService = new ArtService(artRepositoryMock, artMapperMock);
+        when(artRepositoryMock.findAll()).thenReturn(artDtoModels);
+        ArtService artService = new ArtService(artRepositoryMock);
 
         //when
-        List<Art> result = artService.getSearchArtsForPage(name, artist, tehnique, pageNumber, pageSize);
+        List<Art> result = artService.getSearchArtsForPage(name, artist, technique, pageNumber, pageSize);
 
         //then
         assertThat(result.size()).isEqualTo(1);
     }
 
-    private Art createArtModel(String id, String name, String username, List<String> techniques) {
-        return new Art(id, name, username, techniques);
+    private ArtDto createArtModel(String id, String name, String username, List<String> techniques) {
+        ArtDto artDto = new ArtDto();
+        artDto.setId(id);
+        artDto.setName(name);
+        artDto.setArtist_username(username);
+        artDto.setTechniqueCodes(techniques);
+        return artDto;
     }
 }
