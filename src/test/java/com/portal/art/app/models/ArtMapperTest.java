c13040e4808a4eb8b7445881064c94bd5568dbbe
Aleksandra Pajic <aleksandra.pajic.96@gmail.com>
2021-02-06 18:28:50 +0100

    feature/TDD-approach-ArtMapper: step 2. write test

--- src/test/java/com/portal/art/app/models/ArtMapperTest.java
+++ src/test/java/com/portal/art/app/models/ArtMapperTest.java
@@ -1,15 +1,34 @@
 package com.portal.art.app.models;
 
+import com.portal.art.app.repositories.dtos.ArtDto;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class ArtMapperTest {
 
 
     @Test
     public void givenArtDto_whenMapMethodCalled_ThenReturnMappedArtModel() {
+        //given
+        ArtMapper artMapper = new ArtMapper();
+        ArtDto artDto = createArtDto("1", "name1", "artist1");
 
+        //when
+        Art artModel = artMapper.map(artDto);
+
+        //then
+        assertThat(artModel).isNotNull();
+        assertThat(artModel.getId()).isEqualTo("1");
+        assertThat(artModel.getName()).isEqualTo("name1");
+        assertThat(artModel.getArtist_username()).isEqualTo("artist1");
     }
 
+    private ArtDto createArtDto(String id, String name, String artist) {
+        ArtDto artDto = new ArtDto();
+        artDto.setId(id);
+        artDto.setName(name);
+        artDto.setArtist_username(artist);
+        return artDto;
+    }
 }
