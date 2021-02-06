64c46eafbb8d587cac438616bcf96f87d79a1f03
Aleksandra Pajic <aleksandra.pajic.96@gmail.com>
2021-02-06 20:23:17 +0100

    feature/TDD-approach-ArtService: step 2. write test

--- src/test/java/com/portal/art/app/services/ArtServiceTest.java
+++ src/test/java/com/portal/art/app/services/ArtServiceTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 class ArtServiceTest {
@@ -20,14 +21,14 @@ class ArtServiceTest {
         String name = "name";
         String artist = "artist";
         List<ArtDto> dtos = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 3; i++) {
             dtos.add(new ArtDto());
         }
 
         List<Art> artModels = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            artModels.add(new Art());
-        }
+        artModels.add(createArtModel("0", "name0", "artist1", List.of("technique")));
+        artModels.add(createArtModel("1", "test0", "art_test1", List.of("tec_test")));
+        artModels.add(createArtModel("2", "name2", "artist2", List.of("technique2")));
 
         ArtRepository artRepositoryMock = mock(ArtRepository.class);
         when(artRepositoryMock.findAll()).thenReturn(dtos);
@@ -49,14 +50,14 @@ class ArtServiceTest {
         String name = "name";
         String artist = "artist";
         List<ArtDto> dtos = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 3; i++) {
             dtos.add(new ArtDto());
         }
 
         List<Art> artModels = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            artModels.add(new Art());
-        }
+        artModels.add(createArtModel("0", "name0", "artist1", List.of("technique")));
+        artModels.add(createArtModel("1", "test0", "art_test1", List.of("tec_test")));
+        artModels.add(createArtModel("2", "name2", "artist2", List.of("technique2")));
 
         ArtRepository artRepositoryMock = mock(ArtRepository.class);
         when(artRepositoryMock.findAll()).thenReturn(dtos);
@@ -73,6 +74,37 @@ class ArtServiceTest {
 
     @Test
     void givenSearchParameters_whenSearchForArtsCalled_thenFilteredArtModelsAreReturned() {
-       
+        //given
+        String tehnique = "technique";
+        String name = "name";
+        String artist = "artist";
+
+        List<ArtDto> dtos = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            dtos.add(new ArtDto());
+        }
+
+        List<Art> artModels = new ArrayList<>();
+        artModels.add(createArtModel("0", "name0", "artist1", List.of("technique")));
+        artModels.add(createArtModel("1", "test0", "art_test1", List.of("tec_test")));
+        artModels.add(createArtModel("2", "name2", "artist2", List.of("technique2")));
+
+        ArtRepository artRepositoryMock = mock(ArtRepository.class);
+        when(artRepositoryMock.findAll()).thenReturn(dtos);
+        ArtMapper artMapperMock = mock(ArtMapper.class);
+        when(artMapperMock.map(dtos)).thenReturn(artModels);
+        ArtService artService = new ArtService(artRepositoryMock, artMapperMock);
+
+        //when
+        List<Art> result = artService.searchForArts(tehnique, name, artist);
+
+        //then
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result.get(0).getId()).isEqualTo("0");
+        assertThat(result.get(1).getId()).isEqualTo("2");
+    }
+
+    private Art createArtModel(String id, String name, String username, List<String> techniques){
+        return new Art(id, name, username, techniques);
     }
 }
