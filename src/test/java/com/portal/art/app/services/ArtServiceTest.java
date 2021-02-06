b87a712fd3e9b0c5b43a8d7e096e46b6a504507b
Aleksandra Pajic <aleksandra.pajic.96@gmail.com>
2021-02-06 15:29:17 +0100

    feature/TDD-approach-ArtService: step 1. create test

---
+++ src/test/java/com/portal/art/app/services/ArtServiceTest.java
@@ -0,0 +1,11 @@
+package com.portal.art.app.services;
+
+import org.junit.jupiter.api.Test;
+
+class ArtServiceTest {
+
+    @Test
+    void givenSearchParameters_whenSearchForArtsCalled_thenAllArtsFromRepositoryAreRetrieved() {
+        
+    }
+}
