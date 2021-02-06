2955576539cf252e262c8cbbe64e8011b8e37439
Aleksandra Pajic <aleksandra.pajic.96@gmail.com>
2021-02-06 18:00:23 +0100

    feature/TDD-approach-ArtService: step 2. write test

--- src/test/java/com/portal/art/app/services/ArtServiceTest.java
+++ src/test/java/com/portal/art/app/services/ArtServiceTest.java
@@ -1,5 +1,7 @@
 package com.portal.art.app.services;
 
+import com.portal.art.app.models.Art;
+import com.portal.art.app.models.ArtMapper;
 import com.portal.art.app.repositories.ArtRepository;
 import com.portal.art.app.repositories.dtos.ArtDto;
 import org.junit.jupiter.api.Test;
@@ -35,6 +37,32 @@ class ArtServiceTest {
 
     @Test
     void givenSearchParameters_whenSearchForArtsCalled_thenAllArtDtosAreMappedToArtModel() {
-        
+        //given
+        String tehnique = "technique";
+        String name = "name";
+        String artist = "artist";
+        List<ArtDto> dtos = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            dtos.add(new ArtDto());
+        }
+
+        List<Art> artModels = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            artModels.add(new Art());
+        }
+
+        ArtRepository artRepositoryMock = mock(ArtRepository.class);
+        when(artRepositoryMock.findAll()).thenReturn(dtos);
+
+        ArtMapper artMapperMock = mock(ArtMapper.class);
+        when(artMapperMock.map(dtos)).thenReturn(artModels);
+
+        ArtService artService = new ArtService(artRepositoryMock);
+
+        //when
+        artService.searchForArts(tehnique, name, artist);
+
+        //then
+        verify(artMapperMock, times(1)).map(eq(dtos));
     }
 }
