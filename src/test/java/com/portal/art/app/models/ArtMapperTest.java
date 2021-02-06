e1fc4514d1ce7ae194d1a6161007f8395a9d5003
Aleksandra Pajic <aleksandra.pajic.96@gmail.com>
2021-02-06 18:04:28 +0100

    feature/TDD-approach-ArtMapper: step 1. create test

---
+++ src/test/java/com/portal/art/app/models/ArtMapperTest.java
@@ -0,0 +1,15 @@
+package com.portal.art.app.models;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArtMapperTest {
+
+
+    @Test
+    public void givenArtDto_whenMapMethodCalled_ThenReturnMappedArtModel() {
+
+    }
+
+}
